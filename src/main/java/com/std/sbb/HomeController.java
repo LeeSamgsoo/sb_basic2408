0acc0a3acdc81467143bcda01d5e2eeffec8f2ac
Leesamgsoo <sslee3680035@gmail.com>
2024-08-22 11:50:22 +0900

    four basic operation

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -2,15 +2,52 @@ package com.std.sbb;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+    private int num;
+
+    public HomeController() {
+        num = -1;
+    }
+
+    @GetMapping("/home/plus")
+    @ResponseBody
+    public int showPlus(@RequestParam int a, @RequestParam int b) {
+        return a + b;
+    }
+
+    @GetMapping("/home/minus")
+    @ResponseBody
+    public int showMinus(@RequestParam int a, @RequestParam int b) {
+        return a - b;
+    }
+
+    @GetMapping("/home/multiply")
+    @ResponseBody
+    public int showMultiply(@RequestParam int a, @RequestParam int b) {
+        return a * b;
+    }
+
+    @GetMapping("/home/divide")
+    @ResponseBody
+    public int showDivide(@RequestParam int a, @RequestParam int b) {
+        return a / b;
+    }
+
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int getA() {
+        num++;
+        return num;
+    }
 
     @GetMapping("/home/main")
     @ResponseBody
     public String ShowMain() {
-        return "메인페이지";
+        return "안녕하세요";
     }
 
     @GetMapping("/home/main2")
