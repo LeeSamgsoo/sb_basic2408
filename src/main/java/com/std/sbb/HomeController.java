6f9b7fe41e1874ff128ed8e07fe220b235bc15e1
Leesamgsoo <sslee3680035@gmail.com>
2024-08-23 11:04:16 +0900

    spring boot basic lecture 13

--- src/main/java/com/std/sbb/HomeController.java
+++ src/main/java/com/std/sbb/HomeController.java
@@ -1,145 +1,55 @@
 package com.std.sbb;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @Controller
 public class HomeController {
-    private int num;
+    private ArrayList<Person> personArrayList;
 
     public HomeController() {
-        num = -1;
+        personArrayList = new ArrayList<>();
     }
 
-    @GetMapping("/home/return/array")
+    @GetMapping("/home/addPerson")
     @ResponseBody
-    public int[] showArray() {
-        int[] arr = new int[]{10, 20, 30};
-        return arr;
+    public String addPerson(@RequestParam String name, @RequestParam int age) {
+        Person p = new Person(name, age);
+        personArrayList.add(p);
+        System.out.println(p);
+        return p.getId() + "번 사람이 추가되었습니다.";
     }
 
-    @GetMapping("/home/return/list")
+    @GetMapping("/home/people")
     @ResponseBody
-    public List<Integer> showList() {
-        List<Integer> list = new ArrayList<>() {{
-            add(20);
-            add(20);
-            add(30);
-        }};
-        return list;
+    public ArrayList<Person> returnPeople() {
+        return personArrayList;
     }
 
-    @GetMapping("/home/return/map")
-    @ResponseBody
-    public Map<String, Object> showMap() {
-        Map<String, Object> map = new LinkedHashMap<>() {{
-            put("id", 1);
-            put("speed", 100);
-            put("name", "카니발");
-            put("relatedIds", new ArrayList<>() {{
-                add(2);
-                add(3);
-                add(4);
-            }});
-        }};
-        return map;
-    }
-
-    @GetMapping("/home/return/car")
-    @ResponseBody
-    public Car showCar() {
-        Car car = new Car(1, 100, "카니발",
-                new ArrayList<>() {{
-                    add(2);
-                    add(3);
-                    add(4);
-                }});
-        return car;
-    }
-
-    @GetMapping("/home/plus")
-    @ResponseBody
-    public int showPlus(@RequestParam int a, @RequestParam int b) {
-        return a + b;
-    }
-
-    @GetMapping("/home/minus")
-    @ResponseBody
-    public int showMinus(@RequestParam int a, @RequestParam int b) {
-        return a - b;
-    }
-
-    @GetMapping("/home/multiply")
-    @ResponseBody
-    public int showMultiply(@RequestParam int a, @RequestParam int b) {
-        return a * b;
-    }
-
-    @GetMapping("/home/divide")
-    @ResponseBody
-    public int showDivide(@RequestParam int a, @RequestParam int b) {
-        return a / b;
-    }
-
-    @GetMapping("/home/increase")
-    @ResponseBody
-    public int getA() {
-        num++;
-        return num;
-    }
-
-    @GetMapping("/home/main")
-    @ResponseBody
-    public String ShowMain() {
-        return "안녕하세요";
-    }
-
-    @GetMapping("/home/main2")
-    @ResponseBody
-    public String ShowMain2() {
-        return "반갑습니다.";
-    }
-
-    @GetMapping("/home/main3")
-    @ResponseBody
-    public String ShowMain3() {
-        return "즐거웠습니다.";
-    }
 }
 
-class Car {
+@Getter
+@AllArgsConstructor
+@ToString
+class Person {
+    private static int lastId;
     private int id;
-    private int speed;
     private String name;
-    private List<Integer> relatedIds;
+    private int age;
 
-    public Car(int id, int speed, String name, List<Integer> relatedIds) {
-        this.id = id;
-        this.speed = speed;
-        this.name = name;
-        this.relatedIds = relatedIds;
+    static {
+        lastId = 0;
     }
 
-    public int getId() {
-        return id;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public List<Integer> getRelatedIds() {
-        return relatedIds;
+    Person(String name, int age) {
+        this(++lastId, name, age);
     }
 }
+
